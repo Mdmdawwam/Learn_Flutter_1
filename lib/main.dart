d672886425623ca4c99c817ecb93b7d9212d6a15
Mdmdawwam <mdmdawwam@gmail.com>
2025-02-22 17:06:52 +0700

    Update latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:profil/loginpage.dart';
 import 'package:profil/navigator.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Navigation(),
+    home: Loginpage(),
   ));
 }
